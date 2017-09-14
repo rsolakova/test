5a8000b20d441511c08aac41e80765071b966819
rsolakova <rsolakova@yahoo.com>
2017-09-14 16:17:16 +0300

    Added two buttons

---
+++ butoni-smqna-cvetove.js
@@ -0,0 +1,9 @@
+function start() {
+    document.getElementById('start').classList.remove('start');
+    document.getElementById('stop').classList.add('stop');
+}
+
+function stop() {
+    document.getElementById('stop').classList.remove('stop');
+    document.getElementById('start').classList.add('start');
+}
