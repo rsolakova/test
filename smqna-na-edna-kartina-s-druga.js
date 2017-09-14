6edbc2ab7f9984cdc764b7203e33b5620cc51548
rsolakova <rsolakova@yahoo.com>
2017-09-14 16:21:39 +0300

    Added two pictures.

---
+++ smqna-na-edna-kartina-s-druga.js
@@ -0,0 +1,7 @@
+function smeniKartinkata(daQSmenqLi) {
+    if (daQSmenqLi) {
+        document.images[0].src = 'http://www.pngmart.com/files/1/Cat-PNG-HD.png';
+    } else {
+        document.images[0].src = 'http://nebula.wsimg.com/ddaf4cba581f3acb21b541028382651f?AccessKeyId=7AB77D0C3703B086C02A&disposition=0&alloworigin=1';
+    }
+}
